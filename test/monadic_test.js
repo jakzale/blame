e0732f5f629611802fe1880b7d31fc46b5d4cff3
Jakub Z <jakzale@gmail.com>
2014-04-18 15:53:05 +0200

    Add test, clean code

--- test/monadic_test.js
+++ test/monadic_test.js
@@ -7,75 +7,68 @@ define(['monadic','chai'], function(monadic, chai) {
   monadic();
   var expect = chai.expect;
 
+  // Helper for generating closures
+  function closure (type, value, label) {
+    return function() {
+      return wrap(type, value, label);
+    };
+  }
+
+  function closed (type, value, label, arg) {
+    return function() {
+      return wrap(type, value, label)(arg);
+    };
+  }
+
   describe('monadic blame', function() {
     it('should be a function', function () {
-      expect(typeof monadic === 'function').to.eql(true);
-      expect(Fun !== undefined).to.eql(true);
+      expect(typeof monadic).to.eql('function');
+      expect(typeof Fun).to.eql('function');
     });
 
     describe('wrap', function() {
       describe('ground types', function() {
-        it ('check numbers', function() {
+        it ('checks numbers', function() {
           // In order to check for errors, you need to pass a closure
-          var example = function(){
-            return wrap(Num, 2, 'p');
-          };
-          var example2 = function(){
-            return wrap(Num, true, 'p');
-          };
+          expect(closure(Num, 2, 'p')).not.to.throw(Error);
+          expect(wrap(Num, 2, 'p')).to.equal(2);
 
-          expect(example).not.to.throw(Error);
-          expect(example()).to.equal(2);
+          expect(closure(Num, '2', 'p')).to.throw(Error);
+        });
 
-          expect(example2).to.throw(Error);
+        it ('checks strings', function() {
+          expect(closure(Str, 'a', 'p')).not.to.throw(Error);
+          expect(wrap(Str, 'a', 'p')).to.eql('a');
+
+          expect(closure(Str, 2, 'p')).to.throw(Error);
+        });
+
+        it ('checks booleans', function() {
+          expect(closure(Bool, true, 'p')).not.to.throw(Error);
+          expect(wrap(Bool, true, 'p')).to.eql(true);
+
+          expect(closure(Bool, 2, 'p')).to.throw(Error);
         });
       });
 
       describe('function types', function() {
         it ('check for a function', function() {
-          var example = function(){
-            return wrap(Fun(Num, Num), 1, 'p');
-          };
-          var empty = function() {};
-          var example2 = function(){
-            return empty;
-          };
+          function empty () {}
 
-          expect(example).to.throw(Error);
-          expect(example2).not.to.throw(Error);
+          expect(closure(Fun(Num, Num), 1, 'p')).to.throw(Error);
+          expect(closure(Fun(Num, Num), empty, 'p')).not.to.throw(Error);
         });
 
         it ('check type', function() {
-          var fun = function(n) {
-            return n + 1;
-          };
+          function fun (n) {return n + 1;}
+          function fun2(n) {return n + '1';}
 
-          var fun2 = function(n) {
-            return n + '1';
-          };
+          expect(wrap(Fun(Num, Num), fun, 'p')(1)).to.equal(2);
 
-          var wrapped = wrap(Fun(Num, Num), fun, 'p');
-          var wrapped2 = wrap(Fun(Num, Num), fun2, 'p');
-
-          var example = function() {
-            return wrapped(1);
-          };
-          expect(example).not.to.throw(Error);
-          expect(example()).to.equal(2);
-
-          var example1 = function() {
-            return wrapped('1');
-          };
-          expect(example1).to.throw('~p');
-          var example2 = function() {
-            return wrapped2(1);
-          };
-          expect(example2).to.throw('p');
-
-          var example3 = function() {
-            return wrapped2('1');
-          };
-          expect(example3).to.throw('~p');
+          expect(closed(Fun(Num, Num), fun, 'p', 1)).not.to.throw(Error);
+          expect(closed(Fun(Num, Num), fun, 'p', '1')).to.throw('~p');
+          expect(closed(Fun(Num, Num), fun2, 'p', 1)).to.throw('p');
+          expect(closed(Fun(Num, Num), fun2, 'p', '1')).to.throw('p');
         });
       });
     });
