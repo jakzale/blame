7e7f2bd19dbcd9f408951fb91bc489a936a88539
Jakub Z <jakzale@gmail.com>
2014-04-22 01:13:30 +0200

    Broke the implementation of foralls

--- test/blame_test.js
+++ test/blame_test.js
@@ -14,9 +14,12 @@ define(['blame','chai'], function(blame, chai) {
     };
   }
 
-  function closed (type, value, label, arg) {
+  function closed (type, value, label) {
     return function() {
-      return wrap(type, value, label)(arg);
+      var args = Array.prototype.slice.call(arguments || []);
+      return function() {
+        return wrap(type, value, label).apply(undefined, args);
+      };
     };
   }
 
@@ -30,81 +33,85 @@ define(['blame','chai'], function(blame, chai) {
       describe('ground types', function() {
         it ('checks numbers', function() {
           // In order to check for errors, you need to pass a closure
-          expect(closure(Num, 2, 'p')).not.to.throw(Error);
+          expect(closure(Num, 2, 'p')).not.to.throw();
           expect(wrap(Num, 2, 'p')).to.equal(2);
 
-          expect(closure(Num, '2', 'p')).to.throw(Error);
+          expect(closure(Num, '2', 'p')).to.throw();
         });
 
         it ('checks strings', function() {
-          expect(closure(Str, 'a', 'p')).not.to.throw(Error);
+          expect(closure(Str, 'a', 'p')).not.to.throw();
           expect(wrap(Str, 'a', 'p')).to.eql('a');
 
-          expect(closure(Str, 2, 'p')).to.throw(Error);
+          expect(closure(Str, 2, 'p')).to.throw();
         });
 
         it ('checks booleans', function() {
-          expect(closure(Bool, true, 'p')).not.to.throw(Error);
+          expect(closure(Bool, true, 'p')).not.to.throw();
           expect(wrap(Bool, true, 'p')).to.eql(true);
 
-          expect(closure(Bool, 2, 'p')).to.throw(Error);
+          expect(closure(Bool, 2, 'p')).to.throw();
         });
       });
 
       describe('function types', function() {
+        var Num_Num = TFun(Num, Num);
+
         it ('check for a function', function() {
           function empty () {}
 
-          expect(closure(TFun(Num, Num), 1, 'p')).to.throw(Error);
-          expect(closure(TFun(Num, Num), empty, 'p')).not.to.throw(Error);
+          expect(closure(Num_Num, 1, 'p')).to.throw();
+          expect(closure(Num_Num, empty, 'p')).not.to.throw();
         });
 
         it ('check type', function() {
           function fun (n) {return n + 1;}
           function fun2(n) {return n + '1';}
 
-          expect(wrap(TFun(Num, Num), fun, 'p')(1)).to.equal(2);
+          expect(wrap(Num_Num, fun, 'p')(1)).to.equal(2);
 
-          expect(closed(TFun(Num, Num), fun, 'p', 1)).not.to.throw(Error);
-          expect(closed(TFun(Num, Num), fun, 'p', '1')).to.throw('~p');
-          expect(closed(TFun(Num, Num), fun2, 'p', 1)).to.throw('p');
-          expect(closed(TFun(Num, Num), fun2, 'p', '1')).to.throw('p');
+          expect(closed(Num_Num, fun, 'p')(1)).not.to.throw();
+          expect(closed(Num_Num, fun, 'p')('1')).to.throw('~p');
+          expect(closed(Num_Num, fun2, 'p')(1)).to.throw('p');
+          expect(closed(Num_Num, fun2, 'p')('1')).to.throw('p');
         });
       });
 
       describe('polymorphic types', function() {
-        it ('checks for a function', function() {
-          function empty () {}
+        var forallX_X_X = Forall('X', TFun(Tyvar('X'), Tyvar('X')));
+        var forallY_Y_Y = Forall('Y', TFun(Tyvar('Y'), Tyvar('Y')));
+        function empty () {}
+        function identity(x) {return x;}
+        function stringify(x) {return new String(x);}
 
-          expect(closure(Forall('X', TFun(Tyvar('X'), Tyvar('X'))), empty,
-                         'p')).not.to.throw(Error);
-          expect(closure(Forall('X', TFun(Tyvar('X'), Tyvar('X'))), 2,
-                         'p')).to.throw('p');
+        it ('checks for a function', function() {
+          expect(closure(forallX_X_X, empty, 'p')).not.to.throw();
+          expect(closure(forallX_X_X, 2, 'p')).to.throw('p');
         });
 
         it ('ensures the invariants', function() {
-          function identity(x) {return x;}
-          function stringify(x) {return String(x);}
 
-          expect(closed(Forall('X', TFun(Tyvar('X'), Tyvar('X'))), identity,
-                        'p', 1)).not.to.throw(Error);
-          expect(closed(Forall('X', TFun(Tyvar('X'), Tyvar('X'))), stringify,
-                        'p', 1)).to.throw(Error);
+          [1, '1', true].forEach(function(value) {
+            expect(closed(forallX_X_X, identity, 'p')(value)).not.to.throw();
+            expect(closed(forallX_X_X, stringify, 'p')(value)).to.throw();
+          });
         });
 
         it ('allows to be wrapped', function() {
-          function identity(x) {return x;}
           // forall X -> X can be instantiated to Num -> Num
-          expect(closed(TFun(Num, Num), wrap(Forall('X', TFun(Tyvar('X'),
-                                                              Tyvar('X'))),
-                                                              identity, 'p'),
-                                                              'q',
-                                                              1)).not.to.throw(Error);
+          var wrapped_identity = wrap(forallX_X_X, identity, 'p');
+          //wrap(forallY_Y_Y, wrapped_identity, 'g')(1);
+          expect(closed(TFun(Num, Num), wrapped_identity, 'q')(1)).not.to.throw();
+          //expect(closed(forallY_Y_Y, wrapped_identity, 'g')(1)).not.to.throw();
+          expect(wrap(TFun(Num, Num), wrapped_identity, 'g')(1)).to.equal(1);
+        });
 
-          expect(wrap(TFun(Num, Num), wrap(Forall('X', TFun(Tyvar('X'),
-                                                            Tyvar('X'))),
-                                                            identity, 'p'),
-                                                            'g')(1)).to.equal(1);
+        it ('allows multiple same Tyvars', function() {
+          var forallX_X_X_X = Forall('X', TFun(Tyvar('X'), Tyvar('X'), Tyvar('X')));
+          function identity2(x, y) {return x;}
+          var clos = closed(forallX_X_X_X, identity2, 'p');
+          expect(clos(1, 1)).not.to.throw();
+          expect(clos(1, 2)).to.throw();
         });
       });
     });
