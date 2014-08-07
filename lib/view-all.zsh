2dd7151c412a852e2fb770553be236d8dc65dd3e
Jakub Z <jakzale@gmail.com>
2014-08-07 22:05:56 +0100

    Add a list of possible test cases

--- lib/view-all.zsh
+++ lib/view-all.zsh
@@ -9,6 +9,7 @@ packages=("${(f)string}")
 for package in $packages; do
     #Try to get the following informations
     TEST=`npm view $package scripts.test 2>/dev/null`
-    [[ -n $TEST ]] && echo "$package\t$TEST" >> passes-tests.txt
-    echo $package
+    REPO=`npm view $package repository.url 2>/dev/null`
+    [[ -n $TEST && -n $REPO ]] && echo "$package\t$TEST\t$REPO" >> passes-tests.txt
+    echo "$package\t$TEST\t$REPO"
 done
